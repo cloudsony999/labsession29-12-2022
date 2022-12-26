a198e6409f2522a961e12aa934149e069e8c5140
cloudsony999 <cloudsony999@gmail.com>
2022-12-26 12:05:04 +0530

    first commit

---
+++ exceptionday1/src/com/example/Test.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.example.logic.DivideANumber;
+
+class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+		System.out.println("the result is " + DivideANumber.divide(100, 10));
+		System.out.println("the result is " + DivideANumber.divide(100, 0));
+		System.out.println("----continue----");
+
+	}
+
+}
---
+++ exceptionday1/src/com/example/logic/DivideANumber.java
@@ -0,0 +1,9 @@
+package com.example.logic;
+
+public class DivideANumber {
+
+	public static int divide(int x, int y) {
+		return x / y;
+	}
+
+}
